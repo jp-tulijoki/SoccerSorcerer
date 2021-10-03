f7700096413091019a92a8cdb797044b55bd6e68
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-10-03 19:19:11 +0300

    get league id from api

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import requests
 from dotenv import dotenv_values
+import json
 
 config = dotenv_values(".env")
 API_KEY = config["API_KEY"]
 
-endpoint = 'leagues'
+def getLeagueID(league_name):
+    url = f"https://v3.football.api-sports.io/leagues?name={league_name}"
+    payload = {}
+    headers = {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    if data["results"] != 1:
+        print("No league or multiple leagues found")
+    return data["response"][0]["league"]["id"]
 
-url = f"https://v3.football.api-sports.io/{endpoint}"
 
-payload = {}
-headers = {
-    'x-rapidapi-key': API_KEY,
-    'x-rapidapi-host': 'v3.football.api-sports.io'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+id = getLeagueID("Veikkausliiga")
+print(id)

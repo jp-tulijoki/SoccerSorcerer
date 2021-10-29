3454373e7a4a2a5de7e1137a135b06b71e184a25
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-10-29 21:27:47 +0300

    add whitenoise middleware

--- soccer_sorcerer_app/settings.py
+++ soccer_sorcerer_app/settings.py
@@ -29,6 +29,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'soccer_sorcerer_app.urls'

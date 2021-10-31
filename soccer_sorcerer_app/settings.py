cf6a83d821714fc5bded2ebeef9a890dba29f432
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-10-31 16:16:03 +0200

    add heroku host

--- soccer_sorcerer_app/settings.py
+++ soccer_sorcerer_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["soccersorcerer.herokuapp.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
